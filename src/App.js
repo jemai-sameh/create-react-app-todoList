4cbb0c5686a0883a6017bc65185955e7afb239b2
jemai-sameh <samehjemai98@gmail.com>
2023-03-25 11:45:25 +0100

    add todoInput and TodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {TodoInput} from "./Components/TodoInput";
+import { TodoList } from './Components/TodoList';
 function App() {
   return (
     <div className="App">
       <h1>React todo app</h1>
+      <TodoInput/>
+      <TodoList/>
+
     </div>
   );
 }
